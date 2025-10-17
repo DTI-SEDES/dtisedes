da527695f47659a357eb1e418ea90ad057e3678d
DTI-SEDES <dionatan-aristimunha@social.rs.gov.br>
2025-10-17 09:44:24 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+// No array soeUsers, adicione:
+{
+    nome: "Cássio Rafael Oliveira Machado",
+    organizacao: "SEDES", 
+    setor: "ASSCOM",
+    matricula: "4874234",
+    // ... outros campos
+}
+
+// No array office365Users, adicione:
+{
+    conta: "cassio-machado@social.rs.gov.br",
+    nome: "Cássio Rafael Oliveira Machado", 
+    matricula: "4874234",
+    setor: "ASSCOM",
+    // ... outros campos
+}
